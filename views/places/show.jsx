c6b4677699278cb4584f7f0add47b233395514de
Elvin Kosuta <elvinkousta6@gmail.com>
2023-04-23 23:51:33 -0700

    commit functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,41 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show(data) {
     return(
         <Def>
             <main>
-                <h1>show page</h1>
+                <div className='container text-center'>
+                    <h1>{data.place.name}</h1>
+                </div>
+                <div className='container text-center row'>
+                    <div className='col'>
+                        <img src={data.place.pic} />
+                    </div>
+                    <div className='col'>
+                        <div className='card'>
+                            <h2 className='card-title'>Rating</h2>
+                            <h5 className='card-text'>Currently Unrated</h5>
+                        </div>
+                        <div className='card'>
+                            <h2 className='card-title'>Description</h2>
+                            <h5 className='card-text'>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h5>
+                        </div>
+                        <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                            Edit
+                        </a>
+                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                            <button type='submit' className='btn btn-danger'>
+                                Delete
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <hr />
+                <div className='container text-center'>
+                    <h2>Comment</h2>
+                    <h5>No comments yet!</h5> 
+                </div>
             </main>
         </Def>
     )

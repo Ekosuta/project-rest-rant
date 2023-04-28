6e276ea0ca26b1f3fd0977538b1f98e4a87dc5b5
Elvin Kosuta <elvinkousta6@gmail.com>
2023-04-27 21:56:08 -0700

    fixed show page and links

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -18,7 +18,7 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
   .catch(err => {
-    console.log(err)
+    console.log('err', err)
     res.render('error404')
   })
 })
@@ -28,7 +28,14 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  res.send('GET /places/:id stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+    res.render('places/show', {place})
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
 })
 
 router.put('/:id', (req, res) => {
